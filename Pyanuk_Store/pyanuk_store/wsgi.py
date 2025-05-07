69cbee6ac7dde565da70383673302335a0f7487c
Pyanuk <144242312+Pyanuk@users.noreply.github.com>
2025-05-07 20:34:39 +0300

    Update wsgi.py

--- Pyanuk_Store/pyanuk_store/wsgi.py
+++ Pyanuk_Store/pyanuk_store/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pyanuk_Store.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyanuk_store.settings')
 
 application = get_wsgi_application()
